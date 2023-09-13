727aec8a1aa1ace687e91bd06b2f34d2f2f18803
vinayak <vinayak1998th@gmail.com>
2023-09-13 14:12:56 -0700

    Updated conda envs

--- conda_envs/import_envs.sh
+++ conda_envs/import_envs.sh
@@ -3,5 +3,5 @@ eval "$(conda shell.bash hook)"
 ls | grep "ENV*" | cut -c 5- | while read -r txt_file ; do
     CURR_ENV=${txt_file%.txt}
     echo "File: $txt_file | Env: $CURR_ENV"
-    conda create --name $CURR_ENV --file $txt_file 
+    conda create --name $CURR_ENV --file ENV_$txt_file 
 done
